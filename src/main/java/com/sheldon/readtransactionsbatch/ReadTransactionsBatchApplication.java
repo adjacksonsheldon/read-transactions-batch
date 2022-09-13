9807fce7ad344d1c634566d61d057685d421d0aa
Sheldon <adjacksonsheldon@gmail.com>
2022-09-13 10:29:04 -0300

    feature: Deixar apenas o job responsável pela geração do arquivo de saída e remover dependências do postgres

--- src/main/java/com/sheldon/readtransactionsbatch/ReadTransactionsBatchApplication.java
+++ src/main/java/com/sheldon/readtransactionsbatch/ReadTransactionsBatchApplication.java
@@ -1,9 +1,12 @@
 package com.sheldon.readtransactionsbatch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ReadTransactionsBatchApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sheldon/readtransactionsbatch/repository/transacoes/TransacaoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sheldon.readtransactionsbatch.repository.transacoes;
-
-import com.sheldon.readtransactionsbatch.repository.transacoes.entity.TransacaoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface TransacaoRepository  extends JpaRepository<TransacaoEntity, Long> {
-}
